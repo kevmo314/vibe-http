17319104e9fc77eeafa31ced770935b26c81dea7
Kevin Wang <kevmo314@gmail.com>
2025-03-25 18:35:55 -0400

    updates

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # vibe-http
 
+![hero](hero.png)
+
 A tiny HTTP server that passes the HTTP request to OpenAI's ChatGPT API and returns the response.
 
 Consequentially, each website visitor will get a different experience. To limit the chaos a little
@@ -7,7 +9,20 @@ bit, previous requests and responses are passed to ChatGPT as context.
 
 ## Demo
 
-Hosted at
+Hosted at https://vibehttp.com/. Try it out! Be creative with your paths because the server gets the
+entire packet. For example, check out
+
+- https://vibehttp.com/kevin/what-is-the-meaning-of-life
+- https://vibehttp.com/hotels/nyc
+- https://vibehttp.com/cars/electric
+
+Try some subdomains too, for example
+
+- https://news.vibehttp.com/
+- https://jobs.vibehttp.com/
+- https://recipes.vibehttp.com/blog/winter-salads-for-dogs/
+
+If you like the page, be sure to save it. Because you're not going to see it again...
 
 ## Motivation
 
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"log"
 	"net/http"
 	"net/http/httputil"
 
@@ -16,66 +17,82 @@ func main() {
 	client := openai.NewClient()
 	sessions := map[string][]openai.ChatCompletionMessageParamUnion{}
 
-	if err := http.ListenAndServe(":8081", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		pkt, err := httputil.DumpRequest(r, true)
-		if err != nil {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			return
-		}
+	go http.ListenAndServe(":http", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// upgrade to https
+		http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusMovedPermanently)
+	}))
 
-		completionIDCookie := r.CookiesNamed("completion-id")
-
-		var session []openai.ChatCompletionMessageParamUnion
-		var completionID string
-
-		if len(completionID) > 0 {
-			completionID = completionIDCookie[0].Value
-			session = sessions[completionID]
-		} else {
-			session = []openai.ChatCompletionMessageParamUnion{
-				openai.SystemMessage("You are an HTTP web server. The user will send you raw HTTP packets. Respond with only HTML, which will be sent to the user. Embed your CSS and JavaScript and be creative and verbose with your output, you want to make a good impression on the user and users love fancy effects. Subsequent GET and POST requests will also be sent to you so feel free to include links or forms. Do not include images and do not wrap your output in ``` tags."),
+	if err := http.ListenAndServeTLS(":https",
+		"/etc/letsencrypt/live/vibehttp.com/fullchain.pem",
+		"/etc/letsencrypt/live/vibehttp.com/privkey.pem",
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("%s %s", r.Method, r.URL.Path)
+			pkt, err := httputil.DumpRequest(r, true)
+			if err != nil {
+				w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
 			}
-			completionID = uuid.NewString()
-			http.SetCookie(w, &http.Cookie{
-				Name:  "completion-id",
-				Value: completionID,
+
+			completionIDCookie := r.CookiesNamed("completion-id")
+
+			var session []openai.ChatCompletionMessageParamUnion
+			var completionID string
+
+			if len(completionID) > 0 {
+				completionID = completionIDCookie[0].Value
+				session = sessions[completionID]
+			} else {
+				session = []openai.ChatCompletionMessageParamUnion{
+					openai.SystemMessage("You are an HTTP web server. The user will send you raw HTTP packets. Respond with only HTML, which will be sent to the user. Embed your CSS and JavaScript and be creative and verbose with your output, you want to make a good impression on the user and users love fancy effects. Subsequent GET and POST requests will also be sent to you so feel free to include links or forms. Do not include images and do not wrap your output in ``` tags."),
+				}
+				completionID = uuid.NewString()
+				http.SetCookie(w, &http.Cookie{
+					Name:  "completion-id",
+					Value: completionID,
+				})
+				sessions[completionID] = session
+			}
+			session = append(session, openai.UserMessage(string(pkt)))
+
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			w.WriteHeader(http.StatusOK)
+
+			completion := client.Chat.Completions.NewStreaming(r.Context(), openai.ChatCompletionNewParams{
+				Messages: session,
+				Model:    openai.ChatModelGPT4o,
 			})
-			sessions[completionID] = session
-		}
-		session = append(session, openai.UserMessage(string(pkt)))
-
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-
-		completion := client.Chat.Completions.NewStreaming(r.Context(), openai.ChatCompletionNewParams{
-			Messages: session,
-			Model:    openai.ChatModelGPT4o,
-		})
-		acc := openai.ChatCompletionAccumulator{}
-		for completion.Next() {
-			chunk := completion.Current()
-			acc.AddChunk(chunk)
-			if _, err := w.Write([]byte(chunk.Choices[0].Delta.Content)); err != nil {
-				break
+			acc := openai.ChatCompletionAccumulator{}
+			for completion.Next() {
+				chunk := completion.Current()
+				acc.AddChunk(chunk)
+				if _, err := w.Write([]byte(chunk.Choices[0].Delta.Content)); err != nil {
+					break
+				}
 			}
-		}
-		if _, err := w.Write(bannerHTML); err != nil {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			return
-		}
-		if err := completion.Close(); err != nil {
-			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(err.Error()))
-			return
-		}
-		session = append(session, openai.AssistantMessage(acc.Choices[0].Message.Content))
-		sessions[completionID] = session
-	})); err != nil {
-		panic(err)
+			if _, err := w.Write(bannerHTML); err != nil {
+				w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			if err := completion.Close(); err != nil {
+				w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte(err.Error()))
+				return
+			}
+			if len(acc.Choices) == 0 {
+				w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("no choices"))
+				return
+			}
+
+			session = append(session, openai.AssistantMessage(acc.Choices[0].Message.Content))
+			sessions[completionID] = session
+		})); err != nil {
+		log.Fatal(err)
 	}
 }
